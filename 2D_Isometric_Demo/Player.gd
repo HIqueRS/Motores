dda7edbda44365b6bc5cf485b4fec2d104d29302
ArielGoulart <31768080+ArielGoulart@users.noreply.github.com>
2019-04-03 01:10:05 -0300

    Ai meu deus
    TUDO GAMBIARRA, MAS TÁ FEITO, SUCESSO. AI MEU DEUS VOU CHORAR

--- 2D_Isometric_Demo/Player.gd
+++ 2D_Isometric_Demo/Player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+onready var hit = get_node("../Reset")
 # This is a demo showing how KinematicBody2D
 # move_and_slide works.
 
@@ -12,14 +13,29 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("move_up"):
 		motion += Vector2(0, -1)
+		$RayCast2D.position.x = -35         
+		$RayCast2D.position.y = -55 # pra cima
+		
 	if Input.is_action_pressed("move_bottom"):
 		motion += Vector2(0, 1)
+		$RayCast2D.position.x = -35         
+		$RayCast2D.position.y = 55 # pra baixo
+		
 	if Input.is_action_pressed("move_left"):
 		motion += Vector2(-1, 0)
+		$RayCast2D.position.x = -70         
+		$RayCast2D.position.y = 0 # esquerda
+
 	if Input.is_action_pressed("move_right"):
 		motion += Vector2(1, 0)
+		$RayCast2D.position.x = 0         
+		$RayCast2D.position.y = 0 #direita
 	
+		
 	motion = motion.normalized() * MOTION_SPEED
 
 	move_and_slide(motion)
-
+	
+	if $RayCast2D.get_collider() == hit:
+     position.x = 100
+     position.y = 100
