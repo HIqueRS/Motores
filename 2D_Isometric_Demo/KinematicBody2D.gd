ecd754b125f11c60586e8ad66a941b347ee02071
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2019-04-08 19:01:33 -0300

    Enemy follow
    now enemy rotate on path, follow player on collision with raycast and if not collide he just follow his path

--- 2D_Isometric_Demo/KinematicBody2D.gd
+++ 2D_Isometric_Demo/KinematicBody2D.gd
@@ -5,6 +5,7 @@ const MOVE_SPEED = 10
 onready var raycast = $RayCast2D
 
 onready var player = get_node("../../../../troll")
+onready var Path = get_node("../Sprite")
 
 func _physics_process(delta):
 	if player == null:
@@ -17,7 +18,14 @@ func _physics_process(delta):
 	if raycast.is_colliding():
 		var coll = raycast.get_collider()
 		if coll.name == "troll":
+			#seta andar no path false
+			Path.top = false
 			var vec_to_player = player.global_position - global_position
 			vec_to_player = vec_to_player.normalized()
 			global_rotation = atan2(vec_to_player.y, vec_to_player.x)
 			move_and_collide(vec_to_player * MOVE_SPEED * delta)
+			#else seta andar no path true
+		else:
+			Path.top = true
+	else:
+		Path.top = true
--- 2D_Isometric_Demo/Sprite.gd
+++ 2D_Isometric_Demo/Sprite.gd
@@ -5,12 +5,10 @@ var top :bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	top = true
 	set_physics_process(true)
 
 func _physics_process(delta):
-	if Input.is_action_pressed("move_up"):
-		top = false
-	if Input.is_action_pressed("move_bottom"):
-		top = true
+	
 	if top :
 		get_parent().set_offset(get_parent().get_offset() +(50*delta))
