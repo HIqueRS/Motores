cc0726dc58caeedb494143dd270b066c8dfbca6a
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2019-04-10 10:50:11 -0300

    Pet free
    agora pro pet te seguir tem q salvar ele antes

--- 2D_Isometric_Demo/Teste.gd
+++ 2D_Isometric_Demo/Teste.gd
@@ -7,13 +7,16 @@ onready var player = get_node("../troll")
 
 #vou meio q dar merge nos nossos dois codigos pq acho q a sprite bugou tudo
 var top :bool
-var tipo :int #vou ter q fazer um switch case na mão pois não tem
+var tipo :int
+var free :bool #eu devia limpar os codigos
+#vou ter q fazer um switch case na mão pois não tem
 #e tbm pq preciso mudar o estado do inimigo de patrulha pra perseguissão e voltar pra base
 # patrulha = 0 perseguir = 1 voltar = 2 moita = 3
 #assim eu vou usar teu codigo de base se der ruim fudeo sasgashau
 
 func _ready():
 	top = true
+	free = false
 	set_physics_process(true)
 	
 func _physics_process(delta):
@@ -24,8 +27,15 @@ func _physics_process(delta):
 	var casto = player.global_position - global_position #não sei oq tu faz aqui
 	casto = casto.normalized()
 	
-	var vec_to_player = player.global_position - global_position
-	vec_to_player = vec_to_player.normalized()
-	global_rotation = atan2(vec_to_player.y, vec_to_player.x)
-	move_and_collide(vec_to_player * MOVE_SPEED * delta)
+	if free:
+		var vec_to_player = player.global_position - global_position
+		vec_to_player = vec_to_player.normalized()
+		global_rotation = atan2(vec_to_player.y, vec_to_player.x)
+		move_and_collide(vec_to_player * MOVE_SPEED * delta)
+	
+
+func _on_Animal_Area_body_entered(body):
+	if body.name == "troll":
+		print("booora")
+		free = true
 	
