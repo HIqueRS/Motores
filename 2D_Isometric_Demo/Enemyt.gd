bbfa96bbe8f120a3db5b3c77c9ade372c3b76d93
DominikCG <44614079+DominikCG@users.noreply.github.com>
2019-05-13 20:16:48 -0300

    teste IA zona de testes
    ta dando problema com a rotação, o raycast funciona mas se o objeto rotaciona o raycast fica zoadaço ~arrumar isso com pog~

--- 2D_Isometric_Demo/Enemyt.gd
+++ 2D_Isometric_Demo/Enemyt.gd
@@ -1,40 +1,42 @@
 extends KinematicBody2D
 
+
 onready var visao = get_node("visao")
 onready var circulo = get_node("circulo")
-onready var player = get_node("../Playert")
 var pega_ele = false
 var distancia_minima = 60
 var move_speed = 30
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+onready var pf = get_node("../Playert")
+onready var ray = get_node("RayCast2D")
 
 func _physics_process(delta):
-	var goto = (player.global_position - global_position).normalized()
-	var distance = global_position.distance_to(player.global_position)
-	if pega_ele:
-		var vec_to_player = player.global_position - global_position
-		vec_to_player = vec_to_player.normalized()
-		global_rotation = atan2(vec_to_player.y, vec_to_player.x)
-		move_and_slide(goto * move_speed )
-	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+	ray.cast_to = pf.global_position - global_position
+	if ray.get_collider() == pf :
+		move_and_slide(pf.global_position - global_position)
+#	var goto = (pf.global_position - global_position).normalized()
+#	var distance = global_position.distance_to(pf.global_position)
+#	if pega_ele:
+#		var vec_to_player = pf.global_position - global_position
+#		vec_to_player = vec_to_player.normalized()
+#		global_rotation = atan2(vec_to_player.y, vec_to_player.x)
+#		move_and_slide(goto * move_speed )
 #	pass
-
-
-func _on_Area2D_body_entered(body):
-	if body != player:
-		return
-	print("achei")
-	pega_ele = true
-	
-	pass # Replace with function body.
-
-
-func _on_Area2D_body_exited(body):
-		if body == player:
-			pega_ele = false
-		return
-		pass # Replace with function body.
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+##func _process(delta):
+##	pass
+#
+#
+#func _on_Area2D_body_entered(body):
+#	if body != pf:
+#		return
+#	print("achei")
+#	pega_ele = true
+#
+#	pass # Replace with function body.
+#
+#
+#func _on_Area2D_body_exited(body):
+#		if body == pf:
+#			pega_ele = false
+#		return
+#		pass # Replace with function body.
---
+++ 2D_Isometric_Demo/test.gd
@@ -0,0 +1,41 @@
+extends KinematicBody2D
+
+onready var pf = get_node("../Playert")
+onready var ray = get_node("RayCast2D")
+
+var pega_ele = false
+var move_speed = 30
+
+func _physics_process(delta):
+	ray.cast_to = pf.global_position - global_position
+	
+	#move_and_slide(pf.global_position - global_position)
+	var goto = (pf.global_position - global_position).normalized()
+	var distance = global_position.distance_to(pf.global_position)
+	if pega_ele && ray.get_collider() == pf :
+		var vec_to_player = pf.global_position - global_position
+		vec_to_player = vec_to_player.normalized()
+		global_rotation = atan2(vec_to_player.y, vec_to_player.x)
+
+		move_and_slide(goto * move_speed )
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+func _on_Area2D_body_entered(body):
+	if body == pf:
+		pega_ele = true
+		print("achei")
+		return
+	
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_exited(body):
+	if body == pf:
+		pega_ele = false
+	return
+	pass # Replace with function body.
