e9092d531c146e9fe1d4398efdc867a42bff56e6
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2019-05-15 15:28:21 -0300

    Telas

--- 2D_Isometric_Demo/Alpha/Fases/Control.gd
+++ 2D_Isometric_Demo/Alpha/Fases/Control.gd
@@ -4,6 +4,39 @@ func _ready():
 	pass
 
 
-func _on_PLAY_pressed():
+
+
+
+func _on_Instrucoes_pressed():
+	
+	get_tree().change_scene("res://Alpha/Telas/Instrucoes.tscn")
+	
+	pass 
+
+
+func _on_Novo_jogo_pressed():
+	
 	get_tree().change_scene("res://Alpha/Fases/FaseTeste.tscn")
+	
+	pass # Replace with function body.
+
+
+func _on_Opcoes_pressed():
+	
+	get_tree().change_scene("res://Alpha/Telas/Opcoes.tscn")
+	
+	pass # Replace with function body.
+
+
+func _on_Creditos_pressed():
+	
+	get_tree().change_scene("res://Alpha/Telas/Creditos.tscn")
+	
+	pass # Replace with function body.
+
+
+func _on_Sair_pressed():
+	
+	get_tree().quit()
+	
 	pass # Replace with function body.
