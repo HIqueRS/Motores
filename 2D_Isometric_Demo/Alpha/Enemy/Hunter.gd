922199062f9746f2234cfffcf75524b99993464f
Pedro Grossmann <pedro.grossmann3@gmail.com>
2019-05-15 21:18:31 -0300

    inimigo seguindo o player e o animal
    Co-Authored-By: Henrique <riquesouzalima_@hotmail.com>

--- 2D_Isometric_Demo/Alpha/Enemy/Hunter.gd
+++ 2D_Isometric_Demo/Alpha/Enemy/Hunter.gd
@@ -5,10 +5,15 @@ var vis_color = Color(1.0, 1.0, 1.0, 0.1)
 var laser_color = Color(1.0, .329, .298)
 
 onready var player = get_node("../Player")
+onready var animal = get_node("../Animal")
 
 var target
 var hit_pos
 var is_chasing = false
+
+var animal_is_on_sight = false
+var player_is_on_sight = false
+
 # Declare member variables here. Examples:
 
 # Called when the node enters the scene tree for the first time.
@@ -23,16 +28,15 @@ func _physics_process(delta):
 	update()
 	if target:
 		aim()
-	if is_chasing:
-		move_and_slide(player.position-global_position)*mov_speed*delta
-
+	if is_chasing and target:
+		move_and_slide(target.position-global_position)
 
 func aim():
 	var space_state = get_world_2d().direct_space_state
 	var result = space_state.intersect_ray(position, target.position, [self], collision_mask) #posicao inicial do raio #até o position global do alvo #ignorando ele mesmo #colidindo com a mask dele
 	if result:
 		hit_pos = result.position
-		if result.collider.name == "Player":
+		if result.collider.name == "Player" or result.collider.name == "Animal":
 			$Sprite.self_modulate.r = 1.0
 			rotation = (target.position - position).angle()
 			if !is_chasing:
@@ -48,6 +52,10 @@ func _draw():
 
 
 func _on_Visibility_body_entered(body):
+	if body.name=="Player":
+		player_is_on_sight = true
+	if body.name=="Animal":
+		animal_is_on_sight = true
 	if body.name=="Player" or body.name == "Animal":
 		print_debug("ENTROOO")
 		if target:
@@ -56,8 +64,17 @@ func _on_Visibility_body_entered(body):
 		$Sprite.self_modulate.r = 1.0
 
 func _on_Visibility_body_exited(body):
+	if body.name=="Player":
+		player_is_on_sight = false
+	if body.name=="Animal":
+		animal_is_on_sight = false
 	if body == target:
-		print_debug("SAIUUUUUU")
-		target = null
-		is_chasing = false
-		$Sprite.self_modulate.r = 0.2
+		if body.name == "Player" and animal_is_on_sight:
+			target = animal
+		elif body.name == "Animal" and player_is_on_sight:
+			target = player
+		else:
+			print_debug("SAIUUUUUU")
+			target = null
+			is_chasing = false
+			$Sprite.self_modulate.r = 0.2
