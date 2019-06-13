41374ac3325528fc339ccbff6aa9abf1bb25079f
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2019-06-12 22:21:00 -0300

    Music stuff

--- 2D_Isometric_Demo/Alpha/Fases/Fase.gd
+++ 2D_Isometric_Demo/Alpha/Fases/Fase.gd
@@ -4,14 +4,23 @@ var next
 var restart = false
 
 func _ready():
+	
 	pass
 	
 func _process(delta):
 	
 	next = get_child(1).next 
 	
+	
+		
+		
+	
 	pass
 
 func restart():
 	restart = true
 	pass
+
+func _on_AudioStreamPlayer_finished():
+	$"AudioStreamPlayer".play()
+	pass # Replace with function body.
