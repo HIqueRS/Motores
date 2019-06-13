cd382cd0bcdb6bbdee22f88e5d07e6e51b3977d7
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2019-06-12 23:01:32 -0300

    rotate to 8 sides

--- 2D_Isometric_Demo/Alpha/Player/Padrao/Base_player.gd
+++ 2D_Isometric_Demo/Alpha/Player/Padrao/Base_player.gd
@@ -90,7 +90,6 @@ func Movement():
 		
 	if Input.is_action_pressed("move_bottom"):
 		motion += Vector2(0, 1)
-		
 		get_child(1).global_rotation = deg2rad(180)
 		get_child(1).get_child(1).global_rotation = 0
 			
@@ -101,6 +100,18 @@ func Movement():
 	if Input.is_action_pressed("move_right"):
 		motion += Vector2(1, 0)
 		get_child(1).global_rotation = deg2rad(90)
+		get_child(1).get_child(1).global_rotation = 0
+	if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right"):
+		get_child(1).global_rotation = deg2rad(45)
+		get_child(1).get_child(1).global_rotation = 0
+	if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_left"):
+		get_child(1).global_rotation = deg2rad(-45)
+		get_child(1).get_child(1).global_rotation = 0
+	if Input.is_action_pressed("move_bottom") and Input.is_action_pressed("move_right"):
+		get_child(1).global_rotation = deg2rad(135)
+		get_child(1).get_child(1).global_rotation = 0
+	if Input.is_action_pressed("move_bottom") and Input.is_action_pressed("move_left"):
+		get_child(1).global_rotation = deg2rad(-135)
 		get_child(1).get_child(1).global_rotation = 0
 	motion = motion.normalized() * MOTION_SPEED
 
