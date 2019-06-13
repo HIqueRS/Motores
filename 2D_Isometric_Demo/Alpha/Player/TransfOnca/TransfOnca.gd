f41388b1ff5bf59aa3ef9a57d117bdb782b35677
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2019-06-13 00:22:14 -0300

    Voltando no tempo forçadamente

---
+++ 2D_Isometric_Demo/Alpha/Player/TransfOnca/TransfOnca.gd
@@ -0,0 +1,16 @@
+extends CollisionShape2D
+
+var MOTION_SPEED = 320
+var go_back = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Timer_timeout():
+	go_back = true;
+	pass # Replace with function body.
