f0c344942284f86cb7f648553a9aa4837b3618e6
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2019-06-27 11:18:29 -0300

    asset fru fru

--- 2D_Isometric_Demo/Alpha/Enemy/Follow.gd
+++ 2D_Isometric_Demo/Alpha/Enemy/Follow.gd
@@ -6,7 +6,15 @@ var NumPos
 
 var aux
 
+export (bool) var loop 
+
+var Module = 1
+
+var KeepFollowing = true
+
 export (int) var velocity
+
+export (bool) var reverse
 
 func _ready():
 	
@@ -19,24 +27,37 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	
 	path(delta,velocity)
 	pass
 
 func path(delta,vel):
-	disable_pos()
-	Pos.get_child(aux).get_child(0).get_child(0).disabled = false
-	rotation = (Pos.get_child(aux).global_position - global_position).angle()
-	var vec_to_pos = Pos.get_child(aux).global_position- global_position
-	vec_to_pos = vec_to_pos.normalized()
-	move_and_collide(vec_to_pos * delta *vel)
+	if KeepFollowing:
+		disable_pos()
+		print(aux)
+		Pos.get_child(aux).get_child(0).get_child(0).disabled = false
+		rotation = (Pos.get_child(aux).global_position - global_position).angle()
+		var vec_to_pos = Pos.get_child(aux).global_position- global_position
+		vec_to_pos = vec_to_pos.normalized()
+		move_and_collide(vec_to_pos * delta *vel)
 	pass
 
 func _on_Pos_body_entered(body):
 	if body.name == "Follow":
-		#Pos.get_child(aux).get_child(0).get_child(0).disabled = true
-		aux += 1
-		if aux > NumPos:
-			aux = 0
+		aux += 1 * Module
+		if aux < 0:
+			Module = 1
+			aux = 1
+			pass
+		elif aux > NumPos:
+			if loop:
+				if reverse:
+					Module = -1
+					aux-=2
+				else:
+					aux = 0
+			else:
+				KeepFollowing = false
 	pass # Replace with function body.
 
 func disable_pos():
